fbf0c1b36d78399845155e4388957d428ce83453
Jeff Pelton <j@jeffpelton.com>
2013-03-20 14:05:23 -0700

    fix syntax error

--- common.js
+++ common.js
@@ -20,7 +20,7 @@ function defaultReaderList() {
     { 'url': 'http://theoldreader.com/feeds/subscribe?url=%s',
       'description': 'The Old Reader'
     },
-    { 'url': https://www.rolio.com/subscribe.aspx?feedUrl=%s',
+    { 'url': 'https://www.rolio.com/subscribe.aspx?feedUrl=%s',
       'description': 'Rolio'
     }
   ];
